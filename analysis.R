6312d063bed5947096b39ec86efc56797b8949db
Noga-Smadar <nogasmadar1@mail.tau.ac.il>
2025-01-11 12:51:13 +0200

    Update analysis.R
    added headings to the scripts to make it clearer to readers

--- analysis.R
+++ analysis.R
@@ -3,8 +3,9 @@
 # assignment by Noga Smadar
 # ANALYSIS
 
-#generate data
+#### CREATE THE DATAFRAME ----
 
+# generate data
 N = 100
 subID <- seq(1, N, 1)
 age <- runif(N, 18, 60)
@@ -13,10 +14,13 @@ rt <- rnorm(N, 3000, 1500)
 depression <- rnorm(N, 50, 20)
 sleep_hours <- rnorm(N, 7, 2)
 
+# enter the data into a dataframe
 df = data.frame(subID, gender, rt, depression, sleep_hours)
 
+#### DESCRIPTIVES WITH FUNCTION ----
+
+# call function
 source("./functions.R")
 
+# use function
 create_descriptives(df, 2, 13)
-
-
